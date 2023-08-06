9104d728e80d37d04f07b420a1a183ddd9b17cb5
NaceurXab <XaboudaX2004@gmail.com>
2023-08-06 14:27:31 +0100

    first react assignment

---
+++ Week1/day3/Practice/firstJSX/first-jsx/src/App.js
@@ -0,0 +1,22 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello dojo
+      </h1>
+      <h2>
+        things i need to do:
+      </h2>
+      <ul>
+        <li>learn react</li>
+        <li>Climb Mt.Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the Dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
