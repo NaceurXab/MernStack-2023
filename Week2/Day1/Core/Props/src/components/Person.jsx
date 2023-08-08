0e4984336c8a33c4c40dac2f028423f59b4cbcb5
NaceurXab <XaboudaX2004@gmail.com>
2023-08-08 20:52:33 +0100

    Props

---
+++ Week2/Day1/Core/Props/src/components/Person.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Person = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.lastName} , {props.firstName}
+      </h1>
+      <p>
+        Age: {props.age}
+      </p>
+      <p>
+        Hair color: {props.hairColor}
+      </p>
+    </div>
+  )
+}
+
+export default Person
